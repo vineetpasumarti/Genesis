252e32369a35e982921e150bb8fd9accf37d5a4c
vineet <vnpasu@gmail.com>
2025-02-16 13:41:56 -0500

    Added Genesis project files

---
+++ examples/drone/TrackModel.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass, field
+import numpy as np
+import scipy as sp
+import casadi as ca
+import matplotlib.pyplot as plt
+
+# import drone3d as d3d
+from interp import spline_interpolant
+
+
+@dataclass
+class PythonMsg:
+    '''
+    base class for creating types and messages in python
+    '''
+    def __setattr__(self,key,value):
+        '''
+        Overloads default atribute-setting functionality
+          to avoid creating new fields that don't already exist
+        This exists to avoid hard-to-debug errors from accidentally
+          adding new fields instead of modifying existing ones
+
+        To avoid this, use:
+        object.__setattr__(instance, key, value)
+        ONLY when absolutely necessary.
+        '''
+        if not hasattr(self,key):
+            raise TypeError (f'Not allowed to add new field "{key}" to class {self}')
+        else:
+            object.__setattr__(self,key,value)
+
+
+
+@dataclass
+class RaceTrack(PythonMsg):
+    '''
+    base class for creating types and messages in python
+    '''
+    s_tag: np.array = field(default = None)
+    x_gates: np.array = field(default = None)
+    NumOfGates: int = 0
+
+    center_spline: sp.interpolate.CubicSpline = field(default = None)
+    xi: sp.interpolate.interp1d = field(default = None)
+    xj: sp.interpolate.interp1d = field(default = None)
+    xk: sp.interpolate.interp1d = field(default = None)
+
+    f_xc: ca.Function = field(default = None)
+    f_xcs: ca.Function = field(default = None)
+    f_xcss: ca.Function = field(default = None)
+    f_xcsss: ca.Function = field(default = None)
+
+    gate_size: float = 1.2 # Size of the gate
+    gate_type: str = 'square' # Type of gate (square, circle, etc.)
+
+    sL: np.array = field(default = None)
+    L: float = 0.0
+
+    sym_s: ca.SX = field(default = None)
+
+    f_T: ca.Function = field(default = None)
+    f_B: ca.Function = field(default = None)
+    f_N: ca.Function = field(default = None)
+    f_k: ca.Function = field(default = None)
+    f_tau: ca.Function = field(default = None)
+    f_gate_factor: ca.Function = field(default = None)
+
+    AirDensity: float = field(default = 1.225)
+    gravity: float = field(default = 9.81)
+
+    s_prev: float = 0.0
+
+
+    def __init__(self, Traj: int):
+
+        match (Traj):
+            case 1:
+                x = np.array([-1.1, 9.2, 9.2, -4.5, -4.5, 4.75, -2.8, -1.1]).reshape(-1,1)
+                y = np.array([-1.6, 06.6, -4, -6, -6, -0.9, 6.8, -1.6]).reshape(-1,1)
+                z = np.array([3.6, 1.0, 1.2, 3.5, 0.8, 1.2, 1.2, 3.6]).reshape(-1,1)
+            case 2:
+                factor = 1.0
+                x = factor*np.array([-5.0, 5.0,  5.0, -5.0,-5.0]).reshape(-1,1)
+                y = factor*np.array([ 0.0, 0.0,  0.0,  0.0, 0.0]).reshape(-1,1)
+                z = factor*np.array([ 5.0, 5.0, 15.0, 15.0, 5.0]).reshape(-1,1)
+            case 3:
+                x = np.array([0.0  ,  5.0, 0.0 , -5.0,   0.0,  5.0,  0.0, -5.0 , 0.0]).reshape(-1,1)
+                y = np.array([-10.0, -5.0, 0.0 ,  5.0,  10.0,  5.0,  0.0, -5.0, -10.0]).reshape(-1,1)
+                z = np.array([10.0 , 20.0, 10.0,  20.0, 10.0, 20.0, 10.0, 20.0 , 10.]).reshape(-1,1)
+            case 4:
+                x = np.array([ 0.0, 10.0, 10.0,  0.0, -10.0, -10.0, 0.0]).reshape(-1,1)
+                y = np.array([ 0.0,  5.0, -5.0,  0.0,   5.0,  -5.0, 0.0]).reshape(-1,1)
+                z = np.array([10.0, 10.0, 10.0, 10.0,  10.0,  10.0, 10.0]).reshape(-1,1)
+        x_gates = np.concatenate([x.T,y.T,z.T]).T
+        self.x_gates = x_gates
+        self.s_tag = np.linspace(0,x.shape[0]-1,self.x_gates.shape[0])
+        self.NumOfGates = x.shape[0]
+
+
+
+
+@dataclass
+class SplineCenterline(RaceTrack):
+    def __init__(self, Traj: int):
+        super().__init__(Traj)
+
+        self.center_spline = sp.interpolate.CubicSpline(self.s_tag, self.x_gates, bc_type = 'periodic')
+
+        self.xi = spline_interpolant(self.s_tag, self.x_gates[:,0], extrapolate = 'linear',
+            bc_type = 'periodic',
+            fast = False)
+        self.xj = spline_interpolant(self.s_tag, self.x_gates[:,1], extrapolate = 'linear',
+            bc_type = 'periodic',
+            fast = False)
+        self.xk = spline_interpolant(self.s_tag, self.x_gates[:,2], extrapolate = 'linear',
+            bc_type = 'periodic',
+            fast = False)
+    
+        sym_s = ca.SX.sym('s')
+        
+        xc = ca.vertcat(self.xi(sym_s), self.xj(sym_s), self.xk(sym_s))
+        self.f_xc    = ca.Function('f_xc',    [sym_s], [xc])
+        self.f_xcs   = ca.Function('f_xcs',   [sym_s], [ca.jacobian(self.f_xc(sym_s), sym_s)])
+        self.f_xcss  = ca.Function('f_xcss',  [sym_s], [ca.jacobian(self.f_xcs(sym_s), sym_s)])
+        self.f_xcsss = ca.Function('f_xcsss', [sym_s], [ca.jacobian(self.f_xcss(sym_s), sym_s)])
+
+        # Calculate length between gates:
+        self.sL = np.zeros(self.x_gates.shape[0])
+        N_ds = 2000
+        ds = 1.0/N_ds
+        self.L = 0
+        for i in range(self.x_gates.shape[0]-1):
+            self.sL[i+1] += self.L
+            for d in np.linspace(0,1,N_ds):
+                deltaS = np.linalg.norm(self.f_xcs(self.sL[i]+d))*ds
+                self.sL[i+1] += deltaS
+                self.L += deltaS
+        
+        self.f_T = ca.Function('f_T', [sym_s], [self.f_xcs(sym_s) / ca.norm_2(self.f_xcs(sym_s))])
+        self.f_B = ca.Function('f_B', [sym_s], [ca.cross(self.f_xcs(sym_s), self.f_xcss(sym_s)) / (ca.norm_2(ca.cross(self.f_xcs(sym_s), self.f_xcss(sym_s))))])
+        self.f_N = ca.Function('f_N', [sym_s], [ca.cross(self.f_B(sym_s), self.f_T(sym_s))])
+        self.f_k = ca.Function('f_k', [sym_s], [ca.norm_2(ca.cross(self.f_xcs(sym_s), self.f_xcss(sym_s)))/ca.fmax(0.001, ca.norm_2(self.f_xcs(sym_s)))**3])
+        self.f_tau = ca.Function('f_tau', [sym_s], [ca.dot(ca.cross(self.f_xcs(sym_s),self.f_xcss(sym_s)),self.f_xcsss(sym_s))/ca.fmax(0.001, ca.norm_2(ca.cross(self.f_xcs(sym_s), self.f_xcss(sym_s))))**2])
+        self.f_gate_factor = spline_interpolant([0.0, 0.5, 1.0], [1.0, 3.0, 1.0], extrapolate = 'linear', bc_type = 'clamped', fast = False)
+
+        return
+
+
+    def global_to_local(self, x: np.array, s_prev: float = None):
+        
+        s = self.s_prev if s_prev is None else s_prev
+        if s > self.NumOfGates-1:
+            s -= self.NumOfGates-1
+        elif s < 0:
+            s += self.NumOfGates-1
+        x_s = self.f_xc(s)
+        ds_sign = 1.0
+        ds_step = 0.01
+        n = np.dot((x-x_s).T, self.f_N(s))
+        b = np.dot((x-x_s).T, self.f_B(s))
+        delta = x - x_s - n*self.f_N(s) - b*self.f_B(s)
+        while np.linalg.norm(delta) > 1e-4 and ds_step > 1e-4:
+            s_dir = self.f_T(s)
+            ds = np.dot((x-x_s).T, s_dir)
+            if ds*ds_sign < 0:
+                ds_sign = -ds_sign
+                ds_step *= 0.5
+            s += ds_sign*ds_step
+
+            n = np.dot((x-x_s).T, self.f_N(s))
+            b = np.dot((x-x_s).T, self.f_B(s))
+            x_s = self.f_xc(s)
+            delta = x - x_s - n*self.f_N(s) - b*self.f_B(s)
+
+            # if s > self.NumOfGates-1:
+            #     s -= self.NumOfGates-1
+            # if s < 0:
+            #     s += self.NumOfGates-1
+        if abs(s-self.NumOfGates+1) < 1e-4:
+            s = 0
+        self.s_prev = s
+        return s, n[0,0], b[0,0]
+
+    def local_to_global(self, s: float, n: float, b: float):
+            
+        return self.f_xc(s) + n*self.f_N(s) + b*self.f_B(s)
+
+    def get_gate_orientation(self, idx):
+        return self.f_T(idx), self.f_N(idx), self.f_B(idx)
+
+    def get_gate_transform(self, idx):
+        return ca.vertcat(
+            self.f_xc(idx),
+            self.f_T(idx),
+            self.f_N(idx),
+            self.f_B(idx)
+        )
+
+    def Plot3D_Track(self, figNum: int = 100):
+        
+        s_vec = np.linspace(0,self.s_tag[-1],1000)
+    # Create a spline for the centerline
+        center_spline = np.zeros((s_vec.shape[0],3))
+        for i in range(s_vec.shape[0]):
+            center_spline[i] = np.array(self.f_xc(s_vec[i])).flatten()
+    
+        T = np.zeros((self.x_gates.shape[0],3))
+        N = np.zeros((self.x_gates.shape[0],3))
+        B = np.zeros((self.x_gates.shape[0],3))
+        for i in range(self.x_gates.shape[0]):
+            T[i,:] = np.array(self.f_T(i)).flatten()
+            N[i,:] = np.array(self.f_N(i)).flatten()
+            B[i,:] = np.array(self.f_B(i)).flatten()
+        
+        fig, ax = plt.subplots(3,2, figsize=(10,10))
+        ax[0,0].remove()
+        ax[1,0].remove()
+
+        ax1 = fig.add_subplot(3,2,(1,3), projection='3d')
+        ax1.plot3D(self.x_gates[:,0], self.x_gates[:,1],self.x_gates[:,2], 'ro', markersize=6)
+        ax1.plot3D(center_spline[:,0], center_spline[:,1], center_spline[:,2], 'g--')
+
+        ax1.grid('on')
+        ax1.set_title('Track')
+        if self.gate_type == 'square':
+            gate = self.gate_size * np.array([[0,1,1],[0,1,-1],[0,-1,-1],[0,-1,1],[0,1,1]]).T
+        elif self.gate_type == 'circle':
+            gate = self.gate_size * np.array([np.zeros(100), np.cos(np.linspace(0,2*np.pi,100)), np.sin(np.linspace(0,2*np.pi,100))])
+
+        for i in range(self.x_gates.shape[0]):
+            theta = np.arctan2(T[i,1], T[i,0])
+            R = np.array([[np.cos(theta), -np.sin(theta), 0],
+                        [np.sin(theta), np.cos(theta), 0],
+                        [0, 0, 1]])
+            gate_i = R @ gate
+            ax1.plot3D(self.x_gates[i,0]+gate_i[0,:], self.x_gates[i,1]+gate_i[1,:], self.x_gates[i,2]+gate_i[2,:], 'm-')
+        ax1.axis('equal')
+
+        return fig, ax, ax1
---
+++ examples/drone/waypoint_env_racing_gates.py
@@ -0,0 +1,354 @@
+import torch
+import math
+import genesis as gs
+from genesis.utils.geom import quat_to_xyz, transform_by_quat, inv_quat, transform_quat_by_quat
+import torch.nn.functional as F
+from TrackModel import SplineCenterline
+from interp import spline_interpolant
+import numpy as np
+from pyquaternion import Quaternion
+
+
+
+def gs_rand_float(lower, upper, shape, device):
+    return (upper - lower) * torch.rand(size=shape, device=device) + lower
+
+
+class HoverEnv:
+    def __init__(self, num_envs, env_cfg, obs_cfg, reward_cfg, command_cfg, show_viewer=False, device="cuda"):
+        self.device = torch.device(device)
+
+        self.num_envs = num_envs
+        self.num_obs = obs_cfg["num_obs"]
+        self.num_privileged_obs = None
+        self.num_actions = env_cfg["num_actions"]
+        self.num_commands = command_cfg["num_commands"]
+
+        self.simulate_action_latency = env_cfg["simulate_action_latency"]
+        self.dt = 0.02  # run in 50hz
+        self.max_episode_length = math.ceil(env_cfg["episode_length_s"] / self.dt)
+
+        self.env_cfg = env_cfg
+        self.obs_cfg = obs_cfg
+        self.reward_cfg = reward_cfg
+        self.command_cfg = command_cfg
+
+        self.obs_scales = obs_cfg["obs_scales"]
+        self.reward_scales = reward_cfg["reward_scales"]
+
+        self.track = SplineCenterline(Traj=1)
+        self.gate_positions = self.track.x_gates
+        self.current_gate_idx = torch.zeros(self.num_envs, dtype=torch.long, device=self.device)
+        self.trajectory = self.track.f_xc
+
+        # create scene
+        self.scene = gs.Scene(
+            sim_options=gs.options.SimOptions(dt=self.dt, substeps=2),
+            viewer_options=gs.options.ViewerOptions(
+                max_FPS=env_cfg["max_visualize_FPS"],
+                camera_pos=(17, -6, 12),
+                camera_lookat=(5, -4, 1),
+                camera_fov=40,
+            ),
+            vis_options=gs.options.VisOptions(n_rendered_envs=10),
+            rigid_options=gs.options.RigidOptions(
+                dt=self.dt,
+                constraint_solver=gs.constraint_solver.Newton,
+                enable_collision=True,
+                enable_joint_limit=True,
+            ),
+            show_viewer=show_viewer,
+        )
+
+        # add plane
+        self.scene.add_entity(gs.morphs.Plane())
+
+        # add gates
+        for i in range(self.track.NumOfGates):
+            gate_orientation = self.track.get_gate_orientation(i)
+            gate_T = np.array(gate_orientation[0]).flatten()  # Convert DM to numpy array
+            gate_quat = Quaternion(matrix=np.column_stack([gate_T, *gate_orientation[1:]])).elements
+
+            self.scene.add_entity(
+                morph=gs.morphs.Mesh(
+                    file="meshes/gate.obj",
+                    scale=0.4,
+                    pos=self.track.x_gates[i],
+                    quat=tuple(gate_quat.tolist()),
+                    fixed=True,
+                    collision=False,
+                ),
+            ),
+
+        # add target
+        if self.env_cfg["visualize_target"]:
+            self.target = self.scene.add_entity(
+                morph=gs.morphs.Mesh(
+                    file="meshes/sphere.obj",
+                    scale=0.35,
+                    fixed=True,
+                    collision=False,
+                ),
+                surface=gs.surfaces.Rough(
+                    diffuse_texture=gs.textures.ColorTexture(
+                        color=(1.0, 0.5, 0.5),
+                    ),
+                ),
+            )
+        else:
+            self.target = None
+
+        # add camera
+        if self.env_cfg["visualize_camera"]:
+            self.cam = self.scene.add_camera(
+                res=(1080, 1080),
+                pos=(17, -6, 12),
+                lookat=(5, -4, 1),
+                fov=40,
+                GUI=False,
+            )
+
+        # add drone
+        self.base_init_pos = torch.tensor(self.env_cfg["base_init_pos"], device=self.device)
+        self.base_init_quat = torch.tensor(self.env_cfg["base_init_quat"], device=self.device)
+        self.inv_base_init_quat = inv_quat(self.base_init_quat)
+        self.drone = self.scene.add_entity(gs.morphs.Drone(file="urdf/drones/cf2x.urdf"))
+
+        # build scene
+        self.scene.build(n_envs=num_envs)
+
+        # prepare reward functions and multiply reward scales by dt
+        self.reward_functions, self.episode_sums = dict(), dict()
+        for name in self.reward_scales.keys():
+            self.reward_scales[name] *= self.dt
+            self.reward_functions[name] = getattr(self, "_reward_" + name)
+            self.episode_sums[name] = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
+
+        # initialize buffers
+        self.obs_buf = torch.zeros((self.num_envs, self.num_obs), device=self.device, dtype=gs.tc_float)
+        self.rew_buf = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
+        self.reset_buf = torch.ones((self.num_envs,), device=self.device, dtype=gs.tc_int)
+        self.episode_length_buf = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
+        self.commands = torch.zeros((self.num_envs, self.num_commands), device=self.device, dtype=gs.tc_float)
+
+        self.actions = torch.zeros((self.num_envs, self.num_actions), device=self.device, dtype=gs.tc_float)
+        self.last_actions = torch.zeros_like(self.actions)
+
+        self.base_pos = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.base_quat = torch.zeros((self.num_envs, 4), device=self.device, dtype=gs.tc_float)
+        self.base_lin_vel = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.base_ang_vel = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.last_base_pos = torch.zeros_like(self.base_pos)
+
+        self.extras = dict()  # extra information for logging
+
+        # define target locations
+
+        # self.target_locations = [
+        #     torch.tensor([1.0, 0.0, 1.0], device=self.device),
+        #     torch.tensor([0.0, 1.0, 1.5], device=self.device),
+        #     torch.tensor([-1.0, -1.0, 2.0], device=self.device)
+        # ]
+
+        self.target_locations = [
+            torch.tensor([-1.1, -1.6, 3.6], device=self.device),
+            torch.tensor([9.2, 6.6, 1.0], device=self.device),
+            torch.tensor([9.2, -4, 1.2], device=self.device),
+            torch.tensor([-4.5, -6, 3.5], device=self.device),
+            torch.tensor([-4.5, -6, 0.8], device=self.device),
+            torch.tensor([4.75, -0.9, 1.2], device=self.device),
+            torch.tensor([-2.8, 6.8, 1.2], device=self.device),
+            torch.tensor([-1.1, -1.6, 3.6], device=self.device)
+        ]
+        self.current_target_index = -1 * torch.ones(self.num_envs, dtype=torch.long, device=self.device)
+
+    def _resample_commands(self, envs_idx):
+        # 1. Convert target_locations to pre-stacked tensor
+        target_tensor = torch.stack(self.target_locations)  # Add this line during initialization
+
+        # 2. Use PRE-increment logic
+        new_indices = (self.current_target_index[envs_idx] + 1) % len(self.target_locations)
+        self.commands[envs_idx] = target_tensor[new_indices]  # Direct tensor indexing
+        self.current_target_index[envs_idx] = new_indices  # Update AFTER assignment
+
+        if self.target is not None:
+            self.target.set_pos(self.commands[envs_idx], zero_velocity=True, envs_idx=envs_idx)
+
+    def _at_target(self):
+        at_target = (
+            (torch.norm(self.rel_pos, dim=1) < self.env_cfg["at_target_threshold"]).nonzero(as_tuple=False).flatten()
+        )
+        return at_target
+
+    def step(self, actions):
+        self.actions = torch.clip(actions, -self.env_cfg["clip_actions"], self.env_cfg["clip_actions"])
+        exec_actions = self.actions
+
+        # 14468 is hover rpm
+        self.drone.set_propellels_rpm((1 + exec_actions * 0.8) * 14468.429183500699)
+        self.scene.step()
+
+        # update buffers
+        self.episode_length_buf += 1
+        self.last_base_pos[:] = self.base_pos[:]
+        self.base_pos[:] = self.drone.get_pos()
+        self.rel_pos = self.commands - self.base_pos
+        self.last_rel_pos = self.commands - self.last_base_pos
+        self.base_quat[:] = self.drone.get_quat()
+        self.base_euler = quat_to_xyz(
+            transform_quat_by_quat(torch.ones_like(self.base_quat) * self.inv_base_init_quat, self.base_quat)
+        )
+        inv_base_quat = inv_quat(self.base_quat)
+        self.base_lin_vel[:] = transform_by_quat(self.drone.get_vel(), inv_base_quat)
+        self.base_ang_vel[:] = transform_by_quat(self.drone.get_ang(), inv_base_quat)
+
+        # resample commands
+        envs_idx = self._at_target()
+        self._resample_commands(envs_idx)
+
+        # check if all targets have been reached
+        all_targets_reached = (self.current_target_index == 0).all()
+
+        # check termination and reset
+        self.crash_condition = (
+            (torch.abs(self.base_euler[:, 1]) > self.env_cfg["termination_if_pitch_greater_than"])
+            | (torch.abs(self.base_euler[:, 0]) > self.env_cfg["termination_if_roll_greater_than"])
+            | (torch.abs(self.rel_pos[:, 0]) > self.env_cfg["termination_if_x_greater_than"])
+            | (torch.abs(self.rel_pos[:, 1]) > self.env_cfg["termination_if_y_greater_than"])
+            | (torch.abs(self.rel_pos[:, 2]) > self.env_cfg["termination_if_z_greater_than"])
+            | (self.base_pos[:, 2] < self.env_cfg["termination_if_close_to_ground"])
+        )
+        self.reset_buf = (self.episode_length_buf > self.max_episode_length) | self.crash_condition
+
+        time_out_idx = (self.episode_length_buf > self.max_episode_length).nonzero(as_tuple=False).flatten()
+        self.extras["time_outs"] = torch.zeros_like(self.reset_buf, device=self.device, dtype=gs.tc_float)
+        self.extras["time_outs"][time_out_idx] = 1.0
+
+        self.reset_idx(self.reset_buf.nonzero(as_tuple=False).flatten())
+
+        # compute reward
+        self.rew_buf[:] = 0.0
+        for name, reward_func in self.reward_functions.items():
+            rew = reward_func() * self.reward_scales[name]
+            self.rew_buf += rew
+            self.episode_sums[name] += rew
+
+        # compute observations
+        self.obs_buf = torch.cat(
+            [
+                torch.clip(self.rel_pos * self.obs_scales["rel_pos"], -1, 1),
+                self.base_quat,
+                torch.clip(self.base_lin_vel * self.obs_scales["lin_vel"], -1, 1),
+                torch.clip(self.base_ang_vel * self.obs_scales["ang_vel"], -1, 1),
+                self.last_actions,
+            ],
+            axis=-1,
+        )
+
+        self.last_actions[:] = self.actions[:]
+
+        return self.obs_buf, None, self.rew_buf, self.reset_buf, self.extras
+
+    def get_observations(self):
+        return self.obs_buf
+
+    def get_privileged_observations(self):
+        return None
+
+    def reset_idx(self, envs_idx):
+        if len(envs_idx) == 0:
+            return
+
+        # reset base
+        self.base_pos[envs_idx] = self.base_init_pos
+        self.last_base_pos[envs_idx] = self.base_init_pos
+        self.rel_pos = self.commands - self.base_pos
+        self.last_rel_pos = self.commands - self.last_base_pos
+        self.base_quat[envs_idx] = self.base_init_quat.reshape(1, -1)
+        self.drone.set_pos(self.base_pos[envs_idx], zero_velocity=True, envs_idx=envs_idx)
+        self.drone.set_quat(self.base_quat[envs_idx], zero_velocity=True, envs_idx=envs_idx)
+        self.base_lin_vel[envs_idx] = 0
+        self.base_ang_vel[envs_idx] = 0
+        self.drone.zero_all_dofs_velocity(envs_idx)
+
+        # reset buffers
+        self.last_actions[envs_idx] = 0.0
+        self.episode_length_buf[envs_idx] = 0
+        self.reset_buf[envs_idx] = True
+
+        # fill extras
+        self.extras["episode"] = {}
+        for key in self.episode_sums.keys():
+            self.extras["episode"]["rew_" + key] = (
+                torch.mean(self.episode_sums[key][envs_idx]).item() / self.env_cfg["episode_length_s"]
+            )
+            self.episode_sums[key][envs_idx] = 0.0
+
+        self.current_target_index[envs_idx] = -1
+        self._resample_commands(envs_idx)
+
+    def reset(self):
+        self.reset_buf[:] = True
+        self.reset_idx(torch.arange(self.num_envs, device=self.device))
+        return self.obs_buf, None
+
+    # ------------ reward functions----------------
+    def _reward_progress(self):
+        progress_rew = self.last_rel_pos.norm(dim=1) - self.rel_pos.norm(dim=1)
+        return progress_rew
+
+    def _reward_commands_lrg(self):
+        cmd_lrg_rew = torch.norm(self.actions, dim=1)
+        return cmd_lrg_rew
+
+    def _reward_commands_diff(self):
+        cmd_diff_rew = torch.square(torch.norm(self.actions - self.last_actions, dim=1))
+        return cmd_diff_rew
+
+    def _reward_pass(self):
+        passed = torch.zeros(self.num_envs, device=self.device)
+        passed[self._at_target()] = 1.0 - torch.norm(self.rel_pos[self._at_target()], dim=1)
+        return passed
+
+    def _reward_crash(self):
+        crash = torch.zeros(self.num_envs, device=self.device)
+        crash[self.crash_condition] = -4.0
+        return crash
+
+    def _reward_perception(self):
+        # Transform gate direction to body frame
+        quat = self.base_quat  # (num_envs, 4)
+        gate_dir_world = F.normalize(self.commands - self.base_pos, dim=1)  # Requires F import
+        gate_dir_body = transform_by_quat(gate_dir_world, inv_quat(quat))
+
+        # Camera optical axis in body frame (x-forward)
+        camera_forward_body = torch.tensor([1, 0, 1], device=self.device).repeat(self.num_envs, 1)
+
+        # Angle calculation
+        cos_theta = (camera_forward_body * gate_dir_body).sum(dim=1)
+        angle = torch.acos(torch.clamp(cos_theta, min=-1.0+1e-6, max=1.0-1e-6))
+        return torch.exp(-(angle ** 4))
+
+    # def _reward_target(self):
+    #     target_rew = torch.sum(torch.square(self.last_rel_pos), dim=1) - torch.sum(torch.square(self.rel_pos), dim=1)
+    #     return target_rew
+    #
+    # def _reward_smooth(self):
+    #     smooth_rew = torch.sum(torch.square(self.actions - self.last_actions), dim=1)
+    #     return smooth_rew
+    #
+    # def _reward_yaw(self):
+    #     yaw = self.base_euler[:, 2]
+    #     yaw = torch.where(yaw > 180, yaw - 360, yaw) / 180 * 3.14159  # use rad for yaw_reward
+    #     yaw_rew = torch.exp(self.reward_cfg["yaw_lambda"] * torch.abs(yaw))
+    #     return yaw_rew
+    #
+    # def _reward_angular(self):
+    #     angular_rew = torch.norm(self.base_ang_vel / 3.14159, dim=1)
+    #     return angular_rew
+    #
+    # def _reward_crash(self):
+    #     crash_rew = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
+    #     crash_rew[self.crash_condition] = 1
+    #     return crash_rew
+
